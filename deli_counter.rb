68fbd2309ea9c388feeb1b8182876bfef140bdc8
andy reyes <11710reyes@gmail.com>
2018-12-29 03:40:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(line, next_person)
   line << next_person
   puts "Welcome, #{next_person}. You are number #{line.length} in line."
 end
+
+def now_serving(line)
+  if line.length == 0
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}." 
+  end
+end
